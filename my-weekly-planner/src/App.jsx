b9f3942037deafdd603fd48955df62ab3022bf7c
IkramMaizi <ikrammaizi1@gmail.com>
2025-01-21 23:38:05 +0100

    Initial project setup

---
+++ my-weekly-planner/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold'>My Weekly Planner</h1>
+    </>
+  )
+}
+
+export default App
