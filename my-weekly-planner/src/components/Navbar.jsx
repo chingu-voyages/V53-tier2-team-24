b84de0927f59a207add17d7ba3b30df8cb735a40
IkramMaizi <ikrammaizi1@gmail.com>
2025-01-24 11:33:45 +0100

    fix some bugs for the hamburger menu

--- my-weekly-planner/src/components/Navbar.jsx
+++ my-weekly-planner/src/components/Navbar.jsx
@@ -21,20 +21,32 @@ export default function Navbar() {
           aria-label="Toggle menu"
         >
           <svg
-            className="w-8 h-8 hover:rotate-180 transition-all"
+            className="w-8 h-8 transition-all"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
+            {isOpen ? (
+              // "X" icon
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              // Hamburger icon
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
           </svg>
         </button>
+
 
         <ul className="hidden md:flex flex-row gap-3 ">
           {[
@@ -75,6 +87,7 @@ export default function Navbar() {
                 className={
                   "transition-all duration-300 px-4 py-2 w-full text-righ hover:font-semibold"
                 }
+                onClick={() => setIsOpen(false)}
               >
                 {label}
               </Link>
